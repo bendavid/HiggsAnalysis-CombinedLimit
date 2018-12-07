78d539b450d91022c4e55336ebb1648a39b3e797
Marco Cipriani <cippy91@gmail.com>
2018-12-07 11:42:11 +0100

    adding option --postfix to text2hdf5.py

--- scripts/text2hdf5.py
+++ scripts/text2hdf5.py
@@ -42,8 +42,7 @@ parser.add_option("-S","--doSystematics", type=int, default=1, help="enable syst
 parser.add_option("","--chunkSize", type=int, default=4*1024**2, help="chunk size for hd5fs storage")
 parser.add_option("", "--sparse", default=False, action='store_true',  help="Store normalization and systematics arrays as sparse tensors")
 parser.add_option("", "--scaleMaskedYields", type=float, default=1.,  help="Scaling factor for yields in masked channels")
-#parser.add_option("", "--postfix", default="",type="string", help="add _<postfix> to output hdf5 file")
-#parser.add_option("", "--outputDir", default="",type="string", help="Specify folder for output file (it is created if not existing)")
+parser.add_option("", "--postfix", default="",type="string", help="add _<postfix> to output hdf5 file")
 (options, args) = parser.parse_args()
 
 if len(args) == 0:
@@ -449,6 +448,8 @@ if chunkSize > options.chunkSize:
 outfilename = options.out.replace('.root','.hdf5')
 if options.sparse:
   outfilename = outfilename.replace('.hdf5','_sparse.hdf5')
+if options.postfix: 
+    outfilename = outfilename.replace('.hdf5','_{pf}.hdf5'.format(pf=options.postfix))
 f = h5py_cache.File(outfilename, chunk_cache_mem_size=chunkSize, mode='w')
 
 #save some lists of strings to the file for later use
